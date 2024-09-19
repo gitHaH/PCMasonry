e993b4f4b37d0634e3aec47637601698c564babb
张萌 <1006520276@qq.com>
2024-09-19 15:47:09 +0800

    asd

---
+++ PCMasonry/Classes/ZMB.h
@@ -0,0 +1,17 @@
+//
+//  ZMB.h
+//  CYTest
+//
+//  Created by 张萌 on 2024/9/2.
+//
+
+#import <Foundation/Foundation.h>
+#import "ZMA.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZMB : NSObject<ZMProtol>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PCMasonry/Classes/ZMB.m
@@ -0,0 +1,14 @@
+//
+//  ZMB.m
+//  CYTest
+//
+//  Created by 张萌 on 2024/9/2.
+//
+
+#import "ZMB.h"
+
+@implementation ZMB
+- (NSString *)dealString:(NSString *)oriString{
+    return [NSString stringWithFormat:@"aaa-%@",oriString];
+}
+@end
